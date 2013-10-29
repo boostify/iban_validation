8496885ad3fcb4c4bd53948c7e15f913726c6cf5
Frank C. Eckert <frank.eckert@boost-project.com>
2013-10-29 16:10:07 +0100

    make validator work without

--- spec/iban_validation/known_iban_validator_spec.rb
+++ spec/iban_validation/known_iban_validator_spec.rb
@@ -2,34 +2,40 @@ require 'spec_helper'
 
 module IbanValidation
   describe KnownIbanValidator do
-    class KnownModel
-      include ActiveModel::Validations
-      attr_accessor :iban
-      validates :iban, known_iban: true
+    begin
+      require 'banking_data'
 
-      def initialize(iban)
-        @iban = iban
+      class KnownModel
+        include ActiveModel::Validations
+        attr_accessor :iban
+        validates :iban, known_iban: true
+
+        def initialize(iban)
+          @iban = iban
+        end
       end
-    end
 
-    it 'is invalid' do
-      model = KnownModel.new 'DE'
-      expect(model).to be_invalid
-    end
+      it 'is invalid' do
+        model = KnownModel.new 'DE'
+        expect(model).to be_invalid
+      end
 
-    it 'is invalid' do
-      model = KnownModel.new 'GB82WEST12345698765432'
-      expect(model).to be_invalid
-    end
+      it 'is invalid' do
+        model = KnownModel.new 'GB82WEST12345698765432'
+        expect(model).to be_invalid
+      end
 
-    it 'is valid' do
-      model = KnownModel.new 'DE89370400440532013000'
-      expect(model).to be_valid
-    end
+      it 'is valid' do
+        model = KnownModel.new 'DE89370400440532013000'
+        expect(model).to be_valid
+      end
 
-    it 'is valid' do
-      model = KnownModel.new 'DE15763500000036109724'
-      expect(model).to be_valid
+      it 'is valid' do
+        model = KnownModel.new 'DE15763500000036109724'
+        expect(model).to be_valid
+      end
+    rescue LoadError
+      pending
     end
   end
 end
